9bdd38ec261396abe3a71887900ae676c28ac8e2
Adam Johnson <64501296+dankswoops@users.noreply.github.com>
2022-10-30 13:14:00 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,9 @@
 import Dialect from './components/dialect';
-import Darkmode from './components/darkmode';
-import Prototype from './components/prototype';
 import React from 'react'
-//import './app.css'
 function App(){
-
-  const [toggled, setToggled] = React.useState(false);
-  const handleClick = () => {
-      setToggled((s) => !s);
-  };
-
   return(
     <>
       <Dialect/>
-      <Darkmode toggled={toggled} onClick={handleClick}/>
-      <Prototype/>
-
     </>
   );
 }
